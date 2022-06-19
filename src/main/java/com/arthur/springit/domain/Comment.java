ee95739686a2a3e21cb41c457fdddfb83ae625e1
Halim <33067128+ACMUS3@users.noreply.github.com>
2022-06-19 14:18:11 +0200

    Added abstract class Auditable and extending it in Link and Comment

--- src/main/java/com/arthur/springit/domain/Comment.java
+++ src/main/java/com/arthur/springit/domain/Comment.java
@@ -11,12 +11,13 @@ import javax.persistence.ManyToOne;
 @Entity
 @Data
 @NoArgsConstructor
-public class Comment {
+public class Comment extends Auditable {
 
     @Id
     @GeneratedValue
     private Long id;
     private String body;
+
     @ManyToOne
     private Link link;
 }
